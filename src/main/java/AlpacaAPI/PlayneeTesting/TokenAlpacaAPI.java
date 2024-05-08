8b8c844593d2e41584a4953175ddafe9061bbb70
Zuzu <105684393+GinkuiMain@users.noreply.github.com>
2024-05-08 17:22:47 -0300

    Update TokenAlpacaAPI.java

--- src/main/java/AlpacaAPI/PlayneeTesting/TokenAlpacaAPI.java
+++ src/main/java/AlpacaAPI/PlayneeTesting/TokenAlpacaAPI.java
@@ -3,8 +3,7 @@ package AlpacaAPI.PlayneeTesting;
 public class TokenAlpacaAPI {
 
     public String APIcall()
-        {
-            String APIToken = "EAALeEaqRFIIBOydJAlfZCSwXJpjKF7QXl3YS6ZAXyUHIgqdltwam1TRtNWg14wVynvOWZC0m4fZCbcXAZAlJo5sWZBPczvJi6vY77HPCupZCrZCHik3o17HjrSGzwcZAglqArPEZB8CHqRCTpXRTgoo6RTSUc01Uw2RMZCZAr37taZAB591vihiZBtUyujAZC0F8GkaJov1xyt4Kh4Ak2i6977gHZB0PPif6UgfOZB67n";
-            return APIToken;
+        }
+        // REMOVI POR MOTIVOS DE SEGURANÇA. ESTÁ NO MEU NOTEBOOK.
         }
 }
